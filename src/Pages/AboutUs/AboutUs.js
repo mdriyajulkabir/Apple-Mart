3a2f16cc83100130dc2ba68b815a1efaa5d19198
MD Riyajul Kabir Rakib <mdriyajulkabir@gmail.com>
2021-12-25 13:09:42 +0600

    create aboutus page

---
+++ src/Pages/AboutUs/AboutUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Home/Header/Header";
+
+const AboutUs = () => {
+  return (
+    <div>
+        <Header/>
+      <h2>About us</h2>
+    </div>
+  );
+};
+
+export default AboutUs;
