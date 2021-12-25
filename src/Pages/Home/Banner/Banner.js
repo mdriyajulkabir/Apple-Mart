88696de06e4626f12e93b5dc1dc6f4f567b73b91
MD Riyajul Kabir Rakib <mdriyajulkabir@gmail.com>
2021-12-25 13:06:57 +0600

    update Banner section

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Banner.css";
+import pic2 from "../../../Images/pro_motion_endframe__zk8xkvr4lduy_large.jpg";
+const Banner = () => {
+  return (
+    <>
+      <div className="pb-5">
+        <div className="banne ">
+          <img src={pic2} className="img-fluid" alt="" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
